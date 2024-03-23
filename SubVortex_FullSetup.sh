8ca9deea6acecafc5fb82f221006892b7cd96150
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2024-03-23 09:35:17 -0500

    Update SubVortex_FullSetup.sh

--- SubVortex_FullSetup.sh
+++ SubVortex_FullSetup.sh
@@ -62,7 +62,7 @@ prompt_yes_no() {
     case $yn in
         [Yy]* ) return 0;;
         [Nn]* ) return 1;;
-        * ) echo "Please answer yes/y or no/n."; prompt_yes_no "$prompt";;
+        * ) echo "Please answer yes or no."; prompt_yes_no "$prompt";;
     esac
 }
 
